579b12bc9eb5dd24b480a0b7c994256bfb5081d1
ecorrea95 <e.correa95@hotmail.com>
2022-03-30 18:30:53 -0300

    Agregado country others y nombre de paises

--- js/index.js
+++ js/index.js
@@ -1,15 +1,27 @@
+let regionNames = new Intl.DisplayNames(['en'], {type: 'region'});
+
 const loadCountry = async (name) => {
   try {
     const url = 'https://api.nationalize.io/?name='+name+'';
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
-    document.getElementById("country1").innerHTML = data.country[0].country_id;
-    document.getElementById("country2").innerHTML = data.country[1].country_id;
-    document.getElementById("country3").innerHTML = data.country[2].country_id;
-    document.getElementById("probab1").innerHTML = parseFloat(data.country[0].probability*100).toFixed(2)+"%";
-    document.getElementById("probab2").innerHTML = parseFloat(data.country[1].probability*100).toFixed(2)+"%";
-    document.getElementById("probab3").innerHTML = parseFloat(data.country[2].probability*100).toFixed(2)+"%";
+
+    var c1 = regionNames.of(data.country[0].country_id);
+    var c2 = regionNames.of(data.country[1].country_id);
+    var c3 = regionNames.of(data.country[2].country_id);
+
+    var p1 = parseFloat(data.country[0].probability*100).toFixed(2);
+    var p2 = parseFloat(data.country[1].probability*100).toFixed(2);
+    var p3 = parseFloat(data.country[2].probability*100).toFixed(2);
+
+    document.getElementById("country1").innerHTML = c1;
+    document.getElementById("country2").innerHTML = c2;
+    document.getElementById("country3").innerHTML = c3;
+    document.getElementById("probab1").innerHTML = p1+"%";
+    document.getElementById("probab2").innerHTML = p2+"%";
+    document.getElementById("probab3").innerHTML = p3+"%";
+    document.getElementById("countryOther").innerHTML = "Others";
+    document.getElementById("probabOther").innerHTML = (100-p1-p2-p3).toFixed(2)+"%";
   } catch(err) {
     console.error(err)
   }
@@ -17,6 +29,5 @@ const loadCountry = async (name) => {
 
 function searchCountry() {
   var n = $("#inputName").val();
-  console.log(n);
   loadCountry(n);
 }
