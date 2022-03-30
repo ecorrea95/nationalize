2c5b753be4dd02ab2306012a660e5eb3a05d2da9
ecorrea95 <e.correa95@hotmail.com>
2022-03-29 21:29:47 -0300

    js con var name y try catch

--- js/index.js
+++ js/index.js
@@ -1,19 +1,32 @@
-async function traerPais() {
-  const respuesta = await fetch("https://api.nationalize.io?name=asldkj");
+// async function traerPais() {
+//   var respuesta = await fetch("https://api.nationalize.io/?name=todd");
+//
+//   // if(!respuesta.OK) {
+//   //   let oops = "404";
+//   //   console.log(oops);
+//   //   throw new Error(oops);
+//   // }
+//
+//   const nombre = respuesta.json();
+//
+//   return nombre;
+// }
+//
+// function mostrarNombre(n) {
+//   document.getElementById("mensaje").innerHTML = n.country[0].country_id;
+// }
+//
+// traerPais().then(mostrarNombre);
 
-  if (!respuesta.OK) {
-    let oops = "404";
-    alert(oops);
-    throw new Error(oops);
+
+const loadCountry = async (name) => {
+  try {
+    const url = 'https://api.nationalize.io/?name='+name+'';
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    document.getElementById("mensaje").innerHTML = data.country[0].country_id;
+  } catch(err) {
+    console.error(err)
   }
-
-  const nombre = respuesta.json();
-
-  return nombre;
-}
-
-function mostrarNombre(n) {
-  document.getElementById("mensaje").innerHTML = n.country[0].country_id;
-}
-
-traerPais().then(mostrarNombre);
+};
