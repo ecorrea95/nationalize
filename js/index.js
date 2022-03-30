35b820327e53b3bebdcc7ff6300154f004d6c55e
ecorrea95 <e.correa95@hotmail.com>
2022-03-30 14:03:14 -0300

    Traigo los 3 paises y sus probabilidades

--- js/index.js
+++ js/index.js
@@ -1,31 +1,15 @@
-// async function traerPais() {
-//   var respuesta = await fetch("https://api.nationalize.io/?name=todd");
-//
-//   // if(!respuesta.OK) {
-//   //   let oops = "404";
-//   //   console.log(oops);
-//   //   throw new Error(oops);
-//   // }
-//
-//   const nombre = respuesta.json();
-//
-//   return nombre;
-// }
-//
-// function mostrarNombre(n) {
-//   document.getElementById("mensaje").innerHTML = n.country[0].country_id;
-// }
-//
-// traerPais().then(mostrarNombre);
-
-
 const loadCountry = async (name) => {
   try {
     const url = 'https://api.nationalize.io/?name='+name+'';
     const res = await fetch(url);
     const data = await res.json();
     console.log(data);
-    document.getElementById("mensaje").innerHTML = data.country[0].country_id;
+    document.getElementById("country1").innerHTML = data.country[0].country_id;
+    document.getElementById("country2").innerHTML = data.country[1].country_id;
+    document.getElementById("country3").innerHTML = data.country[2].country_id;
+    document.getElementById("probab1").innerHTML = parseFloat(data.country[0].probability*100).toFixed(2)+"%";
+    document.getElementById("probab2").innerHTML = parseFloat(data.country[1].probability*100).toFixed(2)+"%";
+    document.getElementById("probab3").innerHTML = parseFloat(data.country[2].probability*100).toFixed(2)+"%";
   } catch(err) {
     console.error(err)
   }
