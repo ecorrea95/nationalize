e77667ef6672e6b9523ed7e06e5bb2ecbd8e83bf
ecorrea95 <e.correa95@hotmail.com>
2022-03-31 15:33:04 -0300

    borrador progress

--- js/index.js
+++ js/index.js
@@ -6,26 +6,33 @@ const loadCountry = async (name) => {
     const res = await fetch(url);
     const data = await res.json();
 
-    var c1 = regionNames.of(data.country[0].country_id);
-    var c2 = regionNames.of(data.country[1].country_id);
-    var c3 = regionNames.of(data.country[2].country_id);
+    if (data.country[0].country_id == undefined || data.country[1].country_id == undefined || data.country[2].country_id == undefined) {
+      // Show msg no countries found
+      $("#errorMsg").text("No countries found for that name");
+      $("#error").removeClass("hide");
+    } else {
+      // Show data
+      var c1 = regionNames.of(data.country[0].country_id);
+      var c2 = regionNames.of(data.country[1].country_id);
+      var c3 = regionNames.of(data.country[2].country_id);
 
-    var p1 = parseFloat(data.country[0].probability*100).toFixed(2);
-    var p2 = parseFloat(data.country[1].probability*100).toFixed(2);
-    var p3 = parseFloat(data.country[2].probability*100).toFixed(2);
+      var p1 = parseFloat(data.country[0].probability*100).toFixed(2);
+      var p2 = parseFloat(data.country[1].probability*100).toFixed(2);
+      var p3 = parseFloat(data.country[2].probability*100).toFixed(2);
 
-    $("#contentCenter").removeClass("center");
-    $('div[name="dataPrint"]').removeClass("hide");
-    $("#contentCenter").css("margin-top","10vh");
+      $("#contentCenter").removeClass("center");
+      $('div[name="dataPrint"]').removeClass("hide");
+      $("#contentCenter").css("margin-top","10vh");
 
-    document.getElementById("country1").innerHTML = c1;
-    document.getElementById("country2").innerHTML = c2;
-    document.getElementById("country3").innerHTML = c3;
-    document.getElementById("probab1").innerHTML = p1+"%";
-    document.getElementById("probab2").innerHTML = p2+"%";
-    document.getElementById("probab3").innerHTML = p3+"%";
-    document.getElementById("countryOther").innerHTML = "Others";
-    document.getElementById("probabOther").innerHTML = (100-p1-p2-p3).toFixed(2)+"%";
+      document.getElementById("country1").innerHTML = c1;
+      document.getElementById("country2").innerHTML = c2;
+      document.getElementById("country3").innerHTML = c3;
+      document.getElementById("probab1").innerHTML = p1+"%";
+      document.getElementById("probab2").innerHTML = p2+"%";
+      document.getElementById("probab3").innerHTML = p3+"%";
+      document.getElementById("countryOther").innerHTML = "Others";
+      document.getElementById("probabOther").innerHTML = (100-p1-p2-p3).toFixed(2)+"%";
+    }
   } catch(err) {
     console.error(err)
   }
