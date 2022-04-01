2d5c0112e8eac80f6ac0dd392d8149b5441cc288
ecorrea95 <e.correa95@hotmail.com>
2022-03-31 21:38:31 -0300

    agregado restart animation

--- js/index.js
+++ js/index.js
@@ -7,69 +7,8 @@ const loadCountry = async (name) => {
     const res = await fetch(url);
     const data = await res.json();
 
-    if (data.country[0].country_id == undefined || data.country[1].country_id == undefined || data.country[2].country_id == undefined) {
-      // Show msg no countries found
-      $("#errorMsg").text("No countries found for that name");
-      $("#error").removeClass("hide");
-    } else {
-      // Show data
-      var c1 = regionNames.of(data.country[0].country_id);
-      var c2 = regionNames.of(data.country[1].country_id);
-      var c3 = regionNames.of(data.country[2].country_id);
+    printCountry(data);
 
-      var p1 = parseFloat(data.country[0].probability*100).toFixed(2);
-      root.style.setProperty('--prob1', p1+"%");
-      var p2 = parseFloat(data.country[1].probability*100).toFixed(2);
-      root.style.setProperty('--prob2', p2+"%");
-      var p3 = parseFloat(data.country[2].probability*100).toFixed(2);
-      root.style.setProperty('--prob3', p3+"%");
-      var p4 = (100-p1-p2-p3).toFixed(2);
-      root.style.setProperty('--prob4', p4+"%");
-
-      $("#contentCenter").removeClass("center");
-      $('div[name="dataPrint"]').removeClass("hide");
-      $("#contentCenter").css("margin-top","10vh");
-
-      document.getElementById("country1").innerHTML = c1;
-      document.getElementById("country2").innerHTML = c2;
-      document.getElementById("country3").innerHTML = c3;
-      document.getElementById("countryOther").innerHTML = "Others";
-
-      var probab1 = $('#probab1').text();
-      var probab2 = $('#probab2').text();
-      var probab3 = $('#probab3').text();
-      var probabOther = $('#probabOther').text();
-
-      $({numberValue: probab1}).animate({numberValue: p1}, {
-          duration: 3000,
-          easing: 'linear',
-          step: function() {
-              $('#probab1').text(((this.numberValue*100)/100).toFixed(2)+"%");
-          }
-      });
-      $({numberValue: probab2}).animate({numberValue: p2}, {
-          duration: 3000,
-          easing: 'linear',
-          step: function() {
-              $('#probab2').text(((this.numberValue*100)/100).toFixed(2)+"%");
-          }
-      });
-      $({numberValue: probab3}).animate({numberValue: p3}, {
-          duration: 3000,
-          easing: 'linear',
-          step: function() {
-              $('#probab3').text(((this.numberValue*100)/100).toFixed(2)+"%");
-          }
-      });
-      $({numberValue: probabOther}).animate({numberValue: p4}, {
-          duration: 3000,
-          easing: 'linear',
-          step: function() {
-              $('#probabOther').text(((this.numberValue*100)/100).toFixed(2)+"%");
-          }
-      });
-
-    }
   } catch(err) {
     console.error(err)
   }
@@ -88,6 +27,89 @@ $("#inputName").on('keypress', function(e) {
 
 $("#inputName").focus();
 
-function printCountry() {
+function printCountry(data) {
+  if (data.country[0].country_id == undefined || data.country[1].country_id == undefined || data.country[2].country_id == undefined) {
+    // Show msg no countries found
+    $("#errorMsg").text("No countries found for that name");
+    $("#error").removeClass("hide");
+  } else {
+    // Show data
+    var c1 = regionNames.of(data.country[0].country_id);
+    var c2 = regionNames.of(data.country[1].country_id);
+    var c3 = regionNames.of(data.country[2].country_id);
+
+    reset();
+
+    var p1 = parseFloat(data.country[0].probability*100).toFixed(2);
+    root.style.setProperty('--prob1', p1+"%");
+    var p2 = parseFloat(data.country[1].probability*100).toFixed(2);
+    root.style.setProperty('--prob2', p2+"%");
+    var p3 = parseFloat(data.country[2].probability*100).toFixed(2);
+    root.style.setProperty('--prob3', p3+"%");
+    var p4 = (100-p1-p2-p3).toFixed(2);
+    root.style.setProperty('--prob4', p4+"%");
+
+    $("#contentCenter").removeClass("center");
+    $('div[name="dataPrint"]').removeClass("hide");
+    $("#contentCenter").css("margin-top","10vh");
+
+    document.getElementById("country1").innerHTML = c1;
+    document.getElementById("country2").innerHTML = c2;
+    document.getElementById("country3").innerHTML = c3;
+    document.getElementById("countryOther").innerHTML = "Others";
+
+    var probab1 = $('#probab1').text();
+    var probab2 = $('#probab2').text();
+    var probab3 = $('#probab3').text();
+    var probabOther = $('#probabOther').text();
+
+    $({numberValue: probab1}).animate({numberValue: p1}, {
+        duration: 3000,
+        easing: 'linear',
+        step: function() {
+            $('#probab1').text(((this.numberValue*100)/100).toFixed(2)+"%");
+        }
+    });
+    $({numberValue: probab2}).animate({numberValue: p2}, {
+        duration: 3000,
+        easing: 'linear',
+        step: function() {
+            $('#probab2').text(((this.numberValue*100)/100).toFixed(2)+"%");
+        }
+    });
+    $({numberValue: probab3}).animate({numberValue: p3}, {
+        duration: 3000,
+        easing: 'linear',
+        step: function() {
+            $('#probab3').text(((this.numberValue*100)/100).toFixed(2)+"%");
+        }
+    });
+    $({numberValue: probabOther}).animate({numberValue: p4}, {
+        duration: 3000,
+        easing: 'linear',
+        step: function() {
+            $('#probabOther').text(((this.numberValue*100)/100).toFixed(2)+"%");
+        }
+    });
+  }
+}
+
+function reset() {
+  var el = document.getElementById('progress1');
+  el.style.animation = 'none';
+  el.offsetHeight; /* trigger reflow */
+  el.style.animation = null;
+  var el2 = document.getElementById('progress2');
+  el2.style.animation = 'none';
+  el2.offsetHeight; /* trigger reflow */
+  el2.style.animation = null;
+  var el3 = document.getElementById('progress3');
+  el3.style.animation = 'none';
+  el3.offsetHeight; /* trigger reflow */
+  el3.style.animation = null;
+  var el4 = document.getElementById('progress4');
+  el4.style.animation = 'none';
+  el4.offsetHeight; /* trigger reflow */
+  el4.style.animation = null;
 
 }
